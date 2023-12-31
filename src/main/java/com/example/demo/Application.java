97cfee53af480196c57a2dff821271269495bbf8
Pradeep <pradeepy6331@gmail.com>
2023-12-31 19:01:53 +0530

    m1 & m2 added

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -7,8 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+public void m1(){
+int num=0;
+}
 		SpringApplication.run(Application.class, args);
+<<<<<<< Updated upstream
 public void m3(){
+=======
+public void m2(){
+>>>>>>> Stashed changes
 int num=0;
 }
 	}
